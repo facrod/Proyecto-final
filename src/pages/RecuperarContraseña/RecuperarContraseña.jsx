d16b1a4e15dacc5e6bb33a8e166d4cce55f8410d
Facundo <facundorodriguez494@gmail.com>
2023-07-25 01:40:12 -0300

    solucionado problema de boton

--- src/pages/RecuperarContraseña/RecuperarContraseña.jsx
+++ src/pages/RecuperarContraseña/RecuperarContraseña.jsx
@@ -6,10 +6,10 @@ export const RecuperarContraseña = () => {
     <div className="recContraseña">
       <h4>Recuperar contraseña</h4>
       <p className='opacity-75'>Ingresa tu correo electronico, una vez que aprietes el botón te mandaremos un codigo a tu email para cambiar la contraseña</p>
-      <input type="email" name="" id="" required/>
-      <button className='btn btn-outline-primary my-2'>Enviarme código</button>
+      <input type="email" name="" id="" required className='recInput'/>
+      <button className='btn btn-outline-primary my-2 recBoton'>Enviarme código</button>
       <NavLink to="/">
-        <button className='btn btn-outline-primary my-2'>Volver al inicio</button>
+        <button className='btn btn-outline-primary my-2 recBoton'>Volver al inicio</button>
       </NavLink>
     </div>
   )
